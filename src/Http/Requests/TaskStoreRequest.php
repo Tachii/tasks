3f7f1db01c2873ca2326c482d407f79386c71d0d
glebzaveruha <gzaveruha@best4u.md>
2018-02-01 16:16:32 +0200

    Saving Tasks, removed trait

--- src/Http/Requests/TaskStoreRequest.php
+++ src/Http/Requests/TaskStoreRequest.php
@@ -18,7 +18,7 @@ class TaskStoreRequest extends FormRequest
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
